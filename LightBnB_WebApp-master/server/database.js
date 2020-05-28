2f5cbf55e102b1815e4d96c4397c6b8fed0b7be0
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-28 20:29:27 +0000

    initial implementation of filtering search but code is wet

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -91,12 +91,78 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-    SELECT * 
-    FROM properties
-    LIMIT $1
-  `, [limit])
-    .then(res => res.rows);
+  const queryParams = [];
+  let whereCheck = false;
+  let queryString = `
+   SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+   FROM properties
+   JOIN property_reviews ON properties.id = property_id
+   `;
+
+  if (options.city && whereCheck) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` AND city LIKE $${queryParams.length}`;
+
+  } else if (options.city && whereCheck === false) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+    whereCheck = true;
+  }
+
+
+  if (options.owner_id && whereCheck) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` AND owner_id = $${queryParams.length}`;
+
+  } else if (options.owner_id && whereCheck === false) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `WHERE owner_id = $${queryParams.length}`;
+    whereCheck = true;
+  }
+
+  if (options.minimum_price_per_night && whereCheck) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += ` AND cost_per_night > $${queryParams.length}`;
+
+  } else if (options.minimum_price_per_night && whereCheck === false) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `WHERE cost_per_night > $${queryParams.length}`;
+    whereCheck = true;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+
+    if (whereCheck) {
+      queryString += ` AND cost_per_night < $${queryParams.length}`;
+    } else if (!whereCheck) {
+      queryString += `WHERE cost_per_night < $${queryParams.length}`;
+      whereCheck = true;
+    }
+  }
+
+  queryString += ` 
+    GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `
+    HAVING AVG(property_reviews.rating) > $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length}
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams)
+    .then(res => {
+      return res.rows;
+    });
+  
 };
 exports.getAllProperties = getAllProperties;
 
@@ -113,3 +179,13 @@ const addProperty = function(property) {
   return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
+
+/*
+SELECT properties.*, AVG(property_reviews.rating)
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%Vancouver'
+GROUP BY properties.id
+ORDER BY cost_per_night
+LIMIT 10;
+*/
