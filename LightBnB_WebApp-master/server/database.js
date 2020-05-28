550a05507b308d5cdc2bda78ae7c3949e68be3ee
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-28 16:14:52 +0000

    installed node postgres and implemented a server call to database returning a promise

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  host: 'localhost',
+  database: 'lightbnb',
+  password: 123,
+  port: 5432
+});
 
 /// Users
 
@@ -67,12 +77,13 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+  return pool.query(`
+    SELECT * 
+    FROM properties
+    LIMIT $1
+  `, [limit])
+    .then(res => res.rows);
+};
 exports.getAllProperties = getAllProperties;
 
 
